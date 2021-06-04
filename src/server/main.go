6011e98af1c2a2f99d232862777522ea79016afc
Neeraj Komuravalli <komunee2710@gmail.com>
2021-06-04 20:51:27 +0530

    Changing redis manager import path

--- src/server/main.go
+++ src/server/main.go
@@ -6,9 +6,9 @@ import (
 	"fmt"
 	"net/http"
 
+	redismanager "github.com/NeerajKomuravalli/dummy_workflow_handler_using_go_kafka_neo4j_redis/src/redisManager"
 	"github.com/NeerajKomuravalli/dummy_workflow_handler_using_go_kafka_neo4j_redis/src/server/errors"
 	"github.com/NeerajKomuravalli/dummy_workflow_handler_using_go_kafka_neo4j_redis/src/server/models"
-	redismanager "github.com/NeerajKomuravalli/dummy_workflow_handler_using_go_kafka_neo4j_redis/src/server/redisManager"
 	"github.com/go-playground/validator"
 	"github.com/gorilla/mux"
 )
