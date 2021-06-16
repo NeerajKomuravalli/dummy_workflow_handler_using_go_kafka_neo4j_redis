77bc38542ac93d1c4df70794f7357b8a5824d6ee
Neeraj Komuravalli <komunee2710@gmail.com>
2021-06-16 17:32:49 +0530

    Adding readDeviceData function generator

--- src/neo4jManager/neo4jTransactioFunctionsForDeviceData.go
+++ src/neo4jManager/neo4jTransactioFunctionsForDeviceData.go
@@ -1,4 +1,4 @@
-package main
+package neo4jmanager
 
 import (
 	"github.com/NeerajKomuravalli/dummy_workflow_handler_using_go_kafka_neo4j_redis/src/models"
@@ -29,6 +29,40 @@ func GenerateDeviceDataNodeCreationFn(deviceData models.DeviceData) neo4j.Transa
 		}
 		// You can also retrieve values by name, with e.g. `id, found := record.Get("n.id")`
 		return &models.DeviceData{
+			Id:          record.Values[0].(string),
+			Name:        record.Values[1].(string),
+			Description: record.Values[2].(string),
+			Properties: models.DeviceProperties{
+				Status:     record.Values[3].(string),
+				Temprature: record.Values[4].(float64),
+			},
+		}, nil
+	}
+
+	return generatedFunc
+}
+
+func GenerateGetDeviceDataNodeFn(deviceDataId string) neo4j.TransactionWork {
+	generatedFunc := func(tx neo4j.Transaction) (interface{}, error) {
+		query := "MATCH (n:Device {id: $id}) return n.id, n.name, n.description, n.status, n.temprature"
+		records, err := tx.Run(
+			query,
+			map[string]interface{}{
+				"id": deviceDataId,
+			},
+		)
+		// In face of driver native errors, make sure to return them directly.
+		// Depending on the error, the driver may try to execute the function again.
+		if err != nil {
+			return nil, err
+		}
+
+		record, err := records.Single()
+		if err != nil {
+			return nil, err
+		}
+		// You can also retrieve values by name, with e.g. `id, found := record.Get("n.id")`
+		return models.DeviceData{
 			Id:          record.Values[0].(string),
 			Name:        record.Values[1].(string),
 			Description: record.Values[2].(string),
