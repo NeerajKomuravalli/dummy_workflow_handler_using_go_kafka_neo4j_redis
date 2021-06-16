4e0d0a2c12df27c01370c0c35c6f284aad15faef
Neeraj Komuravalli <komunee2710@gmail.com>
2021-06-16 17:32:12 +0530

    Adding neo4j and server logging parameters

--- src/globalVariables/globalVariables.go
+++ src/globalVariables/globalVariables.go
@@ -5,6 +5,12 @@ const (
 	ServerRedisIpAddress = "localhost:6379"
 	ServerRedisPassword  = ""
 	ServerRedisDbIndex   = 0
+	// Logging
+	ServerLogFolderPath         = "logs"
+	ServerLogFileName           = "logs.txt"
+	ServerLogTimestampFormat    = "02-01-2006 15:04:05"
+	ServerLogFullTimestamp      = true
+	ServerLogFilePermissionCode = 0644
 
 	// Data handler
 	DataHandlerPollDeration   = 10 // In milliseconds
@@ -38,4 +44,9 @@ const (
 	WorkflowLogTimestampFormat    = "02-01-2006 15:04:05"
 	WorkflowLogFullTimestamp      = true
 	WorkflowLogFilePermissionCode = 0644
+
+	// Neo4j
+	Neo4jUri        = "bolt://0.0.0.0:7687"
+	Neo4jDbName     = "neo4j"
+	Neo4jDbPassword = "neo4j-testing"
 )
